ad36ade86c3442afa5066da1de9b57694436e8dc
Dalton Walker <dev@daltonwalker.com>
2024-06-23 20:22:47 -0400

    adds fetch interval

--- tetris_frontend/src/routes/+page.svelte
+++ tetris_frontend/src/routes/+page.svelte
@@ -3,21 +3,57 @@
 	import Score from "$lib/components/Score.svelte";
     import NextTetrominoDisplay from "$lib/components/NextTetrominoDisplay.svelte";
 	import GameMode from "$lib/components/GameMode.svelte";
+    import { onMount } from 'svelte';
     import type { PageData } from './$types';
-    export let data: PageData;
 
-    console.log(data);
+    let board_state: string[][];
+    let next_tetromino_state: string[][];
+    let score: number = 0;
+    let level: number = 0;
+    let lines: number = 0;
+    let game_mode: string;
+
+    const FPS = 60;
+    // const INTERVAL = 1000 / FPS;
+    const INTERVAL = 1500;
+
+    onMount(() => {
+        const interval = setInterval(async () => {
+            const board_response = await fetch('http://localhost:8000/tetris/board');
+            const board_data = await board_response.json();
+            board_state = board_data.board_state;
+
+            const next_tetromino_response = await fetch('http://localhost:8000/tetris/next_tetromino');
+            const next_tetromino_data = await next_tetromino_response.json();
+            next_tetromino_state = next_tetromino_data.next_tetromino;
+
+            const stats_response = await fetch('http://localhost:8000/tetris/statistics');
+            const stats_data = await stats_response.json();
+            score = stats_data.score;
+            level = stats_data.level;
+            lines = stats_data.lines;
+
+            const game_mode_response = await fetch('http://localhost:8000/tetris/game_mode');
+            const game_mode_data = await game_mode_response.json();
+            game_mode = game_mode_data.game_mode;
+        } , INTERVAL);
+        return () => clearInterval(interval);
+    });
 </script>
 
 <main>
     <enhanced:img id="tetris-logo" src="$lib/assets/imgs/tetris-logo.png" alt="Tetris Logo" />
     <div id="game">
         <div id="sidebar">
-            <Score />
-            <NextTetrominoDisplay />
-            <GameMode />
+            <Score score={score} level={level} lines={lines}/>
+            {#key next_tetromino_state}
+                <NextTetrominoDisplay board={next_tetromino_state}/>
+            {/key}
+            <GameMode gameMode = {game_mode}/>
         </div>
-        <Board />
+        {#key board_state}
+            <Board board={board_state}/>
+        {/key}
     </div>
     
 </main>
--- tetris_rl/server.py
+++ tetris_rl/server.py
@@ -1,4 +1,5 @@
-import numpy as np
+import random
+
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
@@ -16,8 +17,45 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+TETROMINO_IDS = ["I", "O", "T", "S", "Z", "J", "L"]
+N_COLS = 10
+N_ROWS = 20
+
+
+def generate_random_board() -> list[list[str]]:
+    return [
+        [random.choice(TETROMINO_IDS) for _ in range(N_COLS)] for _ in range(N_ROWS)
+    ]
+
+
+def generate_random_next_tetromino() -> str:
+    return [[random.choice(TETROMINO_IDS) for _ in range(4)] for _ in range(4)]
+
 
 @app.get("/tetris/board")
 def get_board():
-    json_data = jsonable_encoder(np.random.randint(0, 4, (20, 10)).tolist())
+    random_board = generate_random_board()
+    json_data = jsonable_encoder(random_board)
     return JSONResponse(content={"board_state": json_data})
+
+
+@app.get("/tetris/next_tetromino")
+def get_next_tetromino():
+    next_tetromino = generate_random_next_tetromino()
+    json_data = jsonable_encoder(next_tetromino)
+    return JSONResponse(content={"next_tetromino": json_data})
+
+
+@app.get("/tetris/statistics")
+def get_statistics():
+    score = random.randint(0, 10_000)
+    level = random.randint(0, 100)
+    lines = random.randint(0, 1_000)
+    json_data = jsonable_encoder({"score": score, "level": level, "lines": lines})
+    return JSONResponse(content=json_data)
+
+
+@app.get("/tetris/game_mode")
+def get_game_mode():
+    game_mode = random.choice(["ai", "human"])
+    return JSONResponse(content={"game_mode": game_mode})
